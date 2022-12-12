901246577ae4733362c96fd2fdd429d01be2902b
Mauro Cubilla <mauroscubilla@gmail.com>
2022-12-12 19:10:21 -0300

    prueba pre entrega2

---
+++ stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "alfombra sparco",
+        tipo: " de goma",
+        descripcion: "alfombra de goma de 4 piezas",
+        precio: 10000,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "lampara cree led",
+        tipo: "h4",
+        descripcion: "cree led 30000 lumenes",
+        precio: 20000,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "faros auxiliares",
+        tipo: "led",
+        descripcion: "kit faros rectangulares",
+        precio: 12000,
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "pomo sparco",
+        tipo: "sparco",
+        descripcion: "pomo universal sparco aluminio",
+        precio: 9000,
+        cantidad:0
+    },
+
+    {
+        id: 5,
+        nombre: "soporte sparco",
+        tipo: "telefonia",
+        descripcion: "soporte para celular marca sparco",
+        precio: 6000,
+        cantidad:0
+    },
+
+    {
+        id: 6,
+        nombre: "barra led",
+        tipo: "led ",
+        descripcion: "barra led 80 cm para 4x4",
+        precio: 18000,
+        cantidad:0
+    }
+]
